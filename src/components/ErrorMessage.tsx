8b75393ea93309783b9dccd0204b99f0cbdb8d59
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2026-02-02 13:41:21 +0500

    Create components to handle UI for loading and error states

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+interface ErrorMessageProps {
+    error: string;
+}
+
+const ErrorMessage = ({ error }: ErrorMessageProps) => {
+  return (
+    <div className="h-full flex items-center justify-center">
+        <p className="text-red-700 bg-red-100 p-2 rounded">{error}</p>
+    </div>
+  )
+}
+
+export default ErrorMessage
---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+
+const LoadingSpinner = () => {
+  return (
+    <div className="h-full flex items-center justify-center">
+        <p className="italic">Loading ...</p>
+    </div>
+  )
+}
+
+export default LoadingSpinner
