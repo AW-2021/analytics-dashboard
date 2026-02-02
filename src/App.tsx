cc92a645267b3cf95125ce262b349ff0305effd0
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2026-02-02 13:43:27 +0500

    Wrap App component in QueryClientProvider with client from queryClient.ts & PeriodFilterProvider from context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import Dashboard from "./components/Dashboard";
 import Sidebar from "./components/Sidebar";
+import { PeriodFilterProvider } from "./context/PeriodFilterContext";
 
 function App() {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
   return (
-    <div className={`layout ${isExpanded && 'expanded'}`}>
-      <Sidebar isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
-      <Dashboard />
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <PeriodFilterProvider>
+        <div className={`layout ${isExpanded && "expanded"}`}>
+          <Sidebar isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+          <Dashboard />
+        </div>
+      </PeriodFilterProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
