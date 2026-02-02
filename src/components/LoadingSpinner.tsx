69acee9b8179ce0db0509619be2964d7c6f43a9c
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2026-02-02 18:01:56 +0500

    Install react-spinners package and update LoadingSpinner.tsx to use ClipLoader from new pkg

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,7 +1,14 @@
+import { ClipLoader } from "react-spinners";
+
 const LoadingSpinner = () => {
   return (
     <div className="h-full flex items-center justify-center">
-      <p className="italic">Loading ...</p>
+      <ClipLoader
+        loading={true}
+        color={"#1CA8DD"}
+        size={60}
+        speedMultiplier={0.7}
+      />
     </div>
   );
 };
